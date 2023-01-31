458598cf69da50cc998fb87b41310dc7eb577e7f
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-01-31 16:47:00 +0900

    sample data fetch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+// import './globals.css'
 
 export default function RootLayout({
   children,
@@ -6,13 +6,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="ja">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <header>Header</header>
+        <div className="l-page">{children}</div>
+        <footer>Footer</footer>
+      </body>
     </html>
   )
 }
